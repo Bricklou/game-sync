0bff1d6109f50d0d3d53cc5500046970a9f20c8d
Louis Bailleau <louis.bailleau@free.fr>
2023-08-21 11:57:12 +0200

    refactor: replace `log` with `tracing`

--- server-api/projects/app/src/core/database.rs
+++ server-api/projects/app/src/core/database.rs
@@ -4,6 +4,7 @@ use super::errors::{AppError, AppResult};
 
 pub type DbPool = DatabaseConnection;
 
+#[tracing::instrument("initialize database", skip(database_url))]
 pub async fn init_pool(database_url: &str) -> AppResult<DbPool> {
     let opt = ConnectOptions::new(database_url);
 
--- server-api/projects/app/src/main.rs
+++ server-api/projects/app/src/main.rs
@@ -11,14 +11,13 @@ mod routes;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenvy::dotenv().ok();
-    pretty_env_logger::init();
 
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::TRACE)
         .init();
 
     if let Err(e) = app::run().await {
-        log::error!("{}", e);
+        tracing::error!("{}", e);
     }
 
     Ok(())
