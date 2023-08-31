60d7c6fdf0c2c3eca3f539c90862823793b264e2
Louis Bailleau <louis.bailleau@free.fr>
2023-08-31 15:34:21 +0200

    fix: opaque token session not set properly

--- server-api/projects/actix-multi-session/src/provider/opaque_token.rs
+++ server-api/projects/actix-multi-session/src/provider/opaque_token.rs
@@ -1,4 +1,8 @@
-use actix_web::dev::{ResponseHead, ServiceRequest};
+use actix_web::{
+    dev::{ResponseHead, ServiceRequest},
+    http::header::{HeaderValue, WWW_AUTHENTICATE},
+};
+use anyhow::Context;
 
 use crate::storage::SessionKey;
 
@@ -23,26 +27,37 @@ impl TokenProvider for OpaqueTokenProvider {
         let session_key = headers.get("Authorization")?.to_str().ok()?;
         let session_key = session_key.trim_start_matches("Bearer ");
 
+        println!("Session key: {:?}", session_key);
         session_key.to_owned().try_into().ok()
     }
 
     fn set_session(
         &self,
-        _response: &mut ResponseHead,
-        _session_key: SessionKey,
+        response: &mut ResponseHead,
+        session_key: SessionKey,
     ) -> Result<(), anyhow::Error> {
+        let value: String = session_key.into();
+
+        let src = format!("Bearer {}", value);
+
+        let val = HeaderValue::from_str(&src)
+            .context("Failed to attach a session cookie to the outgoing response")?;
+
+        response.headers_mut().append(WWW_AUTHENTICATE, val);
+
         Ok(())
     }
 
     fn delete_session(&self, _response: &mut ResponseHead) -> Result<(), anyhow::Error> {
+        // Do nothing, unlike cookies, the headers is only sent once, so we don't need to delete it
         Ok(())
     }
 
     fn set_session_cookie_unchanged(
         &self,
-        _response: &mut ResponseHead,
-        _session_key: SessionKey,
+        response: &mut ResponseHead,
+        session_key: SessionKey,
     ) -> Result<(), anyhow::Error> {
-        Ok(())
+        self.set_session(response, session_key)
     }
 }
